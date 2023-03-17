2546f90c915a3e9b6ab999c523203afc2b037b28
Nnadi Victory <chikamagozi25@gmail.com>
2023-03-18 00:21:09 +0100

    display loading spinner while genres are loading

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -4,7 +4,7 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 const GameCardSkeleton = () => {
     return (
         <Card borderRadius={10} overflow='hidden'>
-            <Skeleton height='200px' />
+            <Skeleton height='250px' />
             <CardBody>
                 <SkeletonText />
             </CardBody>
