dd76d149c290c3eaca6d6a0c10026f4222c2e1e3
Nnadi Victory <chikamagozi25@gmail.com>
2023-03-17 21:29:48 +0100

    displaying critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,21 +1,27 @@
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
-import { Game } from "./GameGrid";
-import PlatFormIconList from "./PlatFormIconList";
+import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react';
+import CriticScore from './CriticScore';
+import { Game } from './GameGrid';
+import PlatFormIconList from './PlatFormIconList';
 
 interface Props {
-    game: Game;
-};
+	game: Game;
+}
 
-function GameCard({game}: Props) {
-    return (
-        <Card borderRadius={10} overflow='hidden'>
-            <Image src={game.background_image} />
-            <CardBody>
-                <Heading fontSize='2xl'>{game.name}</Heading>
-                <PlatFormIconList platforms={game.parent_platforms.map(p => p.platform)}/>
-            </CardBody>
-      </Card>
-  )
+function GameCard({ game }: Props) {
+	return (
+		<Card borderRadius={10} overflow='hidden'>
+			<Image src={game.background_image} />
+			<CardBody>
+				<Heading fontSize='2xl'>{game.name}</Heading>
+				<HStack justifyContent='space-between'>
+					<PlatFormIconList
+						platforms={game.parent_platforms.map((p) => p.platform)}
+					/>
+					<CriticScore score={game.metacritic} />
+				</HStack>
+			</CardBody>
+		</Card>
+	);
 }
 
 export default GameCard;
