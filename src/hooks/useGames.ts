d75c43db332f5959c14d71ae12527b6ef9fe2db5
Nnadi Victory <chikamagozi25@gmail.com>
2023-03-17 23:58:27 +0100

    created a more generic data fetching method

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,33 +1,6 @@
-import { useEffect, useState } from 'react';
-import { FetchGameRes, Game } from '../components/GameGrid';
-import { CanceledError } from 'axios';
-import apiClient from '../services/api-client';
+import { Game } from '../components/GameGrid';
+import useData from './useData';
 
-const useGames = () => {
-	const [games, setGames] = useState<Game[]>([]);
-	const [error, setError] = useState('');
-	const [isLoading, setLoading] = useState(false)
-
-	useEffect(() => {
-		const controller = new AbortController();
-
-		setLoading(true)
-		apiClient
-			.get<FetchGameRes>('/games', { signal: controller.signal })
-			.then((res) => {
-				setGames(res.data.results)
-				setLoading(false)
-			})
-			.catch((err) => {
-				if (err instanceof CanceledError) return;
-				setError(err.message);
-				setLoading(false)
-			});
-
-		return () => controller.abort();
-	}, []);
-
-	return { games, error, isLoading };
-};
+const useGames = () => useData<Game>('/games')
 
 export default useGames;
