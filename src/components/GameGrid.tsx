a8ec74175153b41cc6b0f46b90c437eb8086b02a
Nnadi Victory <chikamagozi25@gmail.com>
2023-03-18 15:37:29 +0100

    filter games by genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,6 @@
 import { SimpleGrid, Text } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
+import { Genre } from '../hooks/useGenres';
 import GameCard from './GameCard';
 import GameCardContainer from './GameCardContainer';
 import GameCardSkeleton from './GameCardSkeleton';
@@ -23,8 +24,12 @@ export interface FetchGameRes {
 	results: Game[];
 }
 
-const GameGrid = () => {
-	const { data, error, isLoading } = useGames();
+interface Props {
+	selectedGenre: Genre | null;
+}
+
+const GameGrid = ({selectedGenre}: Props) => {
+	const { data, error, isLoading } = useGames(selectedGenre);
 	const skeletons = [1, 2, 3, 4, 5, 6];
 
 	return (
@@ -33,13 +38,13 @@ const GameGrid = () => {
 			<SimpleGrid columns={{ sm: 2, lg: 3, xl: 4 }} spacing={3} padding='10px'>
 				{isLoading &&
 					skeletons.map((skeleton) => (
-						<GameCardContainer>
-							<GameCardSkeleton key={skeleton} />
+						<GameCardContainer  key={skeleton}>
+							<GameCardSkeleton />
 						</GameCardContainer>
 					))}
 				{data.map((game) => (
-                    <GameCardContainer>
-                        <GameCard key={game.id} game={game} />
+                    <GameCardContainer key={game.id}>
+                        <GameCard  game={game} />
                     </GameCardContainer>
 				))}
 			</SimpleGrid>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,10 @@
 import { Game } from '../components/GameGrid';
 import useData from './useData';
+import { Genre } from './useGenres';
 
-const useGames = () => useData<Game>('/games')
+const useGames = (selectedGenre: Genre | null) =>
+	useData<Game>('/games', { params: { genres: selectedGenre?.id } }, [
+		selectedGenre?.id,
+	]);
 
 export default useGames;
