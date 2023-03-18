7809df20397634b034332ee4ca903ff24aa2bfe0
Nnadi Victory <chikamagozi25@gmail.com>
2023-03-19 00:07:09 +0100

    build search input component

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -26,7 +26,7 @@ const SortSelector = ({onSelectSortOrder, sortOrder}: Props) => {
 			<MenuList>
 				{sortOrders.map((order) => (
 					<MenuItem onClick={() => onSelectSortOrder(order.value)} key={order.value} value={order.value}>
-						Order by: {order.label}
+						{order.label}
 					</MenuItem>
 				))}
 			</MenuList>
