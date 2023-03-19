cf7e4e926025c394ad2148226bcdf72c0898e30c
Nnadi Victory <chikamagozi25@gmail.com>
2023-03-19 16:05:42 +0100

    Refactor game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -34,9 +34,9 @@ const GameGrid = ({gameQuery}: Props) => {
 	const { data, error, isLoading } = useGames(gameQuery);
 	const skeletons = [1, 2, 3, 4, 5, 6];
 
+	if (error) return <Text color='red.500'>{error}</Text>
+
 	return (
-		<>
-			{error && <Text color='red.500'>{error}</Text>}
 			<SimpleGrid columns={{ sm: 2, lg: 3, xl: 4 }} spacing={6} padding='10px'>
 				{isLoading &&
 					skeletons.map((skeleton) => (
@@ -50,7 +50,6 @@ const GameGrid = ({gameQuery}: Props) => {
                     </GameCardContainer>
 				))}
 			</SimpleGrid>
-		</>
 	);
 };
 
