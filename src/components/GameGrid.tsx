267968f319623e6d2908a959e08a9e2b3c017d2f
Nnadi Victory <chikamagozi25@gmail.com>
2023-04-28 23:30:16 +0100

    fixed chakra UI Text componenet ts error

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,7 +1,6 @@
 import { SimpleGrid, Text } from '@chakra-ui/react';
 import { GameQuery } from '../App';
 import useGames from '../hooks/useGames';
-import { Genre } from '../hooks/useGenres';
 import GameCard from './GameCard';
 import GameCardContainer from './GameCardContainer';
 import GameCardSkeleton from './GameCardSkeleton';
@@ -34,7 +33,7 @@ const GameGrid = ({gameQuery}: Props) => {
 	const { data, error, isLoading } = useGames(gameQuery);
 	const skeletons = [1, 2, 3, 4, 5, 6];
 
-	if (error) return <Text color='red.500'>{error}</Text>
+	if (error) return <p color='red.500'>{error}</p>
 
 	return (
 			<SimpleGrid columns={{ sm: 2, lg: 3, xl: 4 }} spacing={6} padding='10px'>
